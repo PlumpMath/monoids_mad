7c9269f4f14741f0f3cc1e1dbe92373dc346340e
Batou99 <lorenzo.lopez@intec.es>
2016-03-31 09:21:18 +0000

    MonoidsInPractice: Step 4. Create ADT for OrderLine

--- src/MonoidsInPractice.hs
+++ src/MonoidsInPractice.hs
@@ -2,53 +2,66 @@ module MonoidsInPractice where
 
 import Text.Printf
 
-data OrderLine = OrderLine {
-  productCode :: String,
-  quantity :: Int,
-  total :: Float
-}
+data OrderLine
+  = ProductLine { productCode :: String, productQuantity :: Int, price :: Float, lineTotal :: Float }
+  | TotalLine { totalQuantity :: Int, orderTotal :: Float }
 
 
 instance Show OrderLine where
-  show orderLine =
-    printf "%-10s %5i %6g" olCode olQuantity olTotal
-    where
-      OrderLine olCode olQuantity olTotal  = orderLine
+  show (ProductLine plCode plQuantity plPrice plTotal) =
+    printf "%-10s %5i @%4g each %6g" plCode plQuantity plPrice plTotal
+  show (TotalLine tlQuantity tlTotal) =
+    printf "%-10s %5i            %6g" "TOTAL" tlQuantity tlTotal
 
 
 sampleLines :: [OrderLine]
 sampleLines =
-  [ OrderLine { productCode = "AAA", quantity = 2, total = 2.99 },
-    OrderLine { productCode = "BBB", quantity = 1, total = 1.99 },
-    OrderLine { productCode = "CCC", quantity = 3, total = 3.99 }
+  [ ProductLine { productCode = "AAA", productQuantity = 2, price = 2.99, lineTotal = 5.98 },
+    ProductLine { productCode = "BBB", productQuantity = 1, price = 1.99, lineTotal = 1.99 },
+    ProductLine { productCode = "CCC", productQuantity = 3, price = 3.99, lineTotal = 11.97 }
   ]
 
 
 moreSampleLines :: [OrderLine]
 moreSampleLines =
-  [ OrderLine { productCode = "DDD", quantity = 4, total = 4.99 },
-    OrderLine { productCode = "BBB", quantity = 1, total = 1.99 },
-    OrderLine { productCode = "EEE", quantity = 2, total = 2.00 }
+  [ ProductLine { productCode = "DDD", productQuantity = 4, price = 4.99, lineTotal = 19.96 },
+    ProductLine { productCode = "BBB", productQuantity = 1, price = 1.99, lineTotal = 1.99 },
+    ProductLine { productCode = "EEE", productQuantity = 2, price = 2.00, lineTotal = 4.00 }
   ]
 
 
 addLine :: OrderLine -> OrderLine -> OrderLine
-addLine (OrderLine "" _ _) line = line
-addLine line (OrderLine "" _ _) = line
+addLine (ProductLine "" _ _ _) line = line
+addLine line (ProductLine "" _ _ _) = line
+addLine (ProductLine _ plQuantity _ plTotal) (TotalLine tlQuantity tlTotal) =
+  TotalLine {
+    totalQuantity = plQuantity + tlQuantity,
+    orderTotal = plTotal + tlTotal
+  }
+addLine (TotalLine tlQuantity tlTotal) (ProductLine _ plQuantity _ plTotal) =
+  TotalLine {
+    totalQuantity = plQuantity + tlQuantity,
+    orderTotal = plTotal + tlTotal
+  }
+addLine (TotalLine tlQuantity1 tlTotal1) (TotalLine tlQuantity2 tlTotal2) =
+  TotalLine {
+    totalQuantity = tlQuantity1 + tlQuantity2,
+    orderTotal = tlTotal1 + tlTotal2
+  }
 addLine line1 line2 =
-  OrderLine {
-    productCode = "TOTAL",
-    quantity = quantity line1 + quantity line2,
-    total = total line1 + total line2
+  TotalLine {
+    totalQuantity = productQuantity line1 + productQuantity line2,
+    orderTotal = lineTotal line1 + lineTotal line2
   }
 
 
 emptyLine :: OrderLine
 emptyLine =
-  OrderLine {
+  ProductLine {
     productCode = "",
-    quantity = 0,
-    total = 0
+    productQuantity = 0,
+    price = 0,
+    lineTotal = 0
   }
 
 
@@ -58,12 +71,13 @@ totalLine = foldl addLine emptyLine
 
 main = do
   mapM_ print sampleLines
-  putStrLn "SUB--------------------"
+  putStrLn "----------------------------------"
   print subtotal
-  putStrLn "-----------------------"
+  putStrLn "----------------------------------"
   mapM_ print moreSampleLines
-  putStrLn "-----------------------"
+  putStrLn "----------------------------------"
   print bigTotal
   where
     subtotal = totalLine sampleLines
     bigTotal = totalLine $ moreSampleLines ++ [subtotal]
+
