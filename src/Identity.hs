d1690773e48923c8806cc9e7898e7adf8712e83d
Batou99 <lorenzo.lopez@intec.es>
2016-03-31 13:29:20 +0000

    Identity: Abstracting out

--- src/Identity.hs
+++ src/Identity.hs
@@ -1,5 +1,6 @@
 module Identity where
 
+import Data.Monoid
 
 data PositiveNumberOrIdentity
   = Positive Int
@@ -12,6 +13,20 @@ instance Monoid PositiveNumberOrIdentity where
   mappend i Zero = i
   mappend (Positive i1) (Positive i2) = Positive (i1 + i2)
 
+-- ABSTRACT THE PATTERN
+
+data Monoid a => NormalOrIdentity a
+  = Normal a
+  | Empty
+
+
+instance Monoid a => Monoid (NormalOrIdentity a) where
+  mempty = Empty
+  mappend Empty x = x
+  mappend x Empty = x
+  mappend (Normal x) (Normal y) = Normal (x <> y)
+
 
 main :: IO()
 main = undefined
+
