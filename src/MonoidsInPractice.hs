6a8e5f4428a58cd36a2e36c582ffe4603262209a
Batou99 <lorenzo.lopez@intec.es>
2016-03-31 09:46:09 +0000

    MonoidsInPractice: Step 6. Use Foldable abstraction to calculate totals

--- src/MonoidsInPractice.hs
+++ src/MonoidsInPractice.hs
@@ -1,72 +1,50 @@
 module MonoidsInPractice where
 
+import Prelude hiding (mapM_)
 import Text.Printf
+import Data.Monoid
+import Data.Foldable
 
-data OrderLine
-  = ProductLine { productCode :: String, productQuantity :: Int, price :: Float, lineTotal :: Float }
-  | TotalLine { totalQuantity :: Int, orderTotal :: Float }
-  | EmptyLine
-
+data OrderLine = OrderLine { productCode :: String, productQuantity :: Int, price :: Float, lineTotal :: Float }
+data TotalLine = TotalLine { totalQuantity :: Int, orderTotal :: Float }
 
 instance Show OrderLine where
-  show (ProductLine plCode plQuantity plPrice plTotal) =
+  show (OrderLine plCode plQuantity plPrice plTotal) =
     printf "%-10s %5i @%4g each %6g" plCode plQuantity plPrice plTotal
+
+instance Show TotalLine where
   show (TotalLine tlQuantity tlTotal) =
     printf "%-10s %5i            %6g" "TOTAL" tlQuantity tlTotal
 
-{-|
-class Monoid m where
-  mempty :: m
-  mappend :: m -> m -> m
-  mconcat :: [m] -> m
-  mconcat = foldr mappend mempty
--}
 
-
-instance Monoid OrderLine where
-  mempty = EmptyLine
-  mappend = addLine
+instance Monoid TotalLine where
+  mempty = TotalLine 0 0
+  mappend line1 line2 = 
+    TotalLine {
+      totalQuantity = totalQuantity line1 + totalQuantity line2,
+      orderTotal = orderTotal line1 + orderTotal line2
+    }
 
 
 sampleLines :: [OrderLine]
 sampleLines =
-  [ ProductLine { productCode = "AAA", productQuantity = 2, price = 2.99, lineTotal = 5.98 },
-    ProductLine { productCode = "BBB", productQuantity = 1, price = 1.99, lineTotal = 1.99 },
-    ProductLine { productCode = "CCC", productQuantity = 3, price = 3.99, lineTotal = 11.97 }
+  [ OrderLine { productCode = "AAA", productQuantity = 2, price = 2.99, lineTotal = 5.98 },
+    OrderLine { productCode = "BBB", productQuantity = 1, price = 1.99, lineTotal = 1.99 },
+    OrderLine { productCode = "CCC", productQuantity = 3, price = 3.99, lineTotal = 11.97 }
   ]
 
 
 moreSampleLines :: [OrderLine]
 moreSampleLines =
-  [ ProductLine { productCode = "DDD", productQuantity = 4, price = 4.99, lineTotal = 19.96 },
-    ProductLine { productCode = "BBB", productQuantity = 1, price = 1.99, lineTotal = 1.99 },
-    ProductLine { productCode = "EEE", productQuantity = 2, price = 2.00, lineTotal = 4.00 }
+  [ OrderLine { productCode = "DDD", productQuantity = 4, price = 4.99, lineTotal = 19.96 },
+    OrderLine { productCode = "BBB", productQuantity = 1, price = 1.99, lineTotal = 1.99 },
+    OrderLine { productCode = "EEE", productQuantity = 2, price = 2.00, lineTotal = 4.00 }
   ]
 
 
-addLine :: OrderLine -> OrderLine -> OrderLine
-addLine EmptyLine line = line
-addLine line EmptyLine = line
-addLine (ProductLine _ plQuantity _ plTotal) (TotalLine tlQuantity tlTotal) =
-  TotalLine {
-    totalQuantity = plQuantity + tlQuantity,
-    orderTotal = plTotal + tlTotal
-  }
-addLine (TotalLine tlQuantity tlTotal) (ProductLine _ plQuantity _ plTotal) =
-  TotalLine {
-    totalQuantity = plQuantity + tlQuantity,
-    orderTotal = plTotal + tlTotal
-  }
-addLine (TotalLine tlQuantity1 tlTotal1) (TotalLine tlQuantity2 tlTotal2) =
-  TotalLine {
-    totalQuantity = tlQuantity1 + tlQuantity2,
-    orderTotal = tlTotal1 + tlTotal2
-  }
-addLine line1 line2 =
-  TotalLine {
-    totalQuantity = productQuantity line1 + productQuantity line2,
-    orderTotal = lineTotal line1 + lineTotal line2
-  }
+toTotalLine :: OrderLine -> TotalLine
+toTotalLine line =
+  TotalLine { totalQuantity = productQuantity line, orderTotal = lineTotal line }
 
 
 main = do
@@ -78,6 +56,6 @@ main = do
   putStrLn "----------------------------------"
   print bigTotal
   where
-    subtotal = mconcat sampleLines
-    bigTotal = mconcat $ moreSampleLines ++ [subtotal]
+    subtotal = foldMap toTotalLine sampleLines
+    bigTotal = foldMap toTotalLine moreSampleLines <> subtotal
 
