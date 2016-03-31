83d18f7202e8b612fb0d0f25ba6c7349f4a0cc02
Batou99 <lorenzo.lopez@intec.es>
2016-03-31 11:43:36 +0000

    MonoidHomomorphisms: Step 1. Basic example

--- src/MonoidHomomorphisms.hs
+++ src/MonoidHomomorphisms.hs
@@ -1,5 +1,22 @@
 module MonoidHomomorphisms  where
 
+import Text.Printf
+
+
+add :: String -> String -> String
+add = (++)
+
+
+wordCount :: String -> Int
+wordCount t =
+  length $ words t
+
+
+page :: String -> Int -> String
+page word numberOfTimes =
+  unwords $ replicate numberOfTimes word
+
 
 main :: IO()
-main = undefined
+main = 
+  printf "The word count is %6i" $ wordCount $ page "Hello" 1000
