49daf654eb3d56e667fad1d610e3451b43840308
Batou99 <lorenzo.lopez@intec.es>
2016-03-31 08:57:44 +0000

    Monoids in practice: Step 1

--- src/MonoidsInPractice.hs
+++ src/MonoidsInPractice.hs
@@ -1,4 +1,37 @@
 module MonoidsInPractice where
 
-main :: IO ()
-main = undefined
+import Text.Printf
+
+data OrderLine = OrderLine {
+  productCode :: String,
+  quantity :: Int,
+  total :: Float
+}
+
+
+instance Show OrderLine where
+  show orderLine =
+    printf "%-10s %5i %6g" olCode olQuantity olTotal
+    where
+      OrderLine olCode olQuantity olTotal  = orderLine
+
+
+sampleLines :: [OrderLine]
+sampleLines =
+  [ OrderLine { productCode = "AAA", quantity = 2, total = 2.99 },
+    OrderLine { productCode = "BBB", quantity = 1, total = 1.99 },
+    OrderLine { productCode = "CCC", quantity = 3, total = 3.99 }
+  ]
+
+
+moreSampleLines :: [OrderLine]
+moreSampleLines =
+  [ OrderLine { productCode = "DDD", quantity = 4, total = 4.99 },
+    OrderLine { productCode = "BBB", quantity = 1, total = 1.99 },
+    OrderLine { productCode = "EEE", quantity = 2, total = 2.00 }
+  ]
+
+
+main = do
+  mapM_ print sampleLines
+  mapM_ print moreSampleLines
