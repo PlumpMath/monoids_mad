91c8c02b9c5f87a47a4525cc5266a0776d3fb789
Batou99 <lorenzo.lopez@intec.es>
2016-03-31 13:26:01 +0000

    NonAssociativeOps: Final Implementation

--- src/NonAssociativeOps.hs
+++ src/NonAssociativeOps.hs
@@ -1,6 +1,19 @@
 module NonAssociativeOps where
 
 import Text.Printf
+import Data.Monoid
+
+
+data Substract = Substract Int deriving (Show)
+
+
+instance Monoid Substract where
+  mempty = Substract 0
+  mappend (Substract a) (Substract b) = Substract (a + b)
+
+
+toInt :: Substract -> Int
+toInt (Substract num) = -num
 
 
 main :: IO ()
@@ -10,6 +23,10 @@ main = do
   printf "Folding from the left: %i\n" $ foldl (-) 0 nums
   -- 1 - (2 - (3 - 0))
   printf "Folding from the right: %i\n" $ foldr (-) 0 nums
+  putStrLn "Now checking Substract typeclass"
+  printf "Folding from the left: %i\n" $ toInt $ foldl mappend mempty $ map Substract nums
+  printf "Folding from the right: %i\n" $ toInt $ foldr mappend mempty $ map Substract nums
+  printf "Folding with foldMap: %i\n" $ toInt $ foldMap Substract nums
 
 
 
